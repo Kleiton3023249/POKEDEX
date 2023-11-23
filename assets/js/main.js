545020c59e26c80f0abf2e9201acd2f8c47164c4
kleiton <kleytonferreira9@gmail.com>
2023-11-23 00:10:09 -0300

    add de botão para chamar mais páginas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,37 @@
 const pokemonList = document.getElementById('pokemons')
+const load = document.getElementById('loadMore')
+const limit = 10
+let offset = 0
+const maxFatch = 135
 
+function loadMorePokemons (offset,limit)
+    {pokeApi.getPokemons(offset,limit).then((pokemons) => {
+    const newList = pokemons.map((pokemon) =>  
+        {return `<li class="${pokemon.types[0]}">
+        <span class="number">${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+        <div class="detail">
+                <img src=${pokemon.photo} alt=${pokemon.name}>
+                
+            <ol class="types"> 
+            ${pokemon.types.map((typeName) => `<li class="type">${typeName}</li>`).join('')}
+            </ol>
+        </div>
+        </li>`}).join('')
+    pokemonList.innerHTML += newList
+    })}
 
-const convertPokemonToHtml = (pokemon) => {
-    
-    return `<li class="${pokemon.types[0]}">
-    <span class="number">${pokemon.number}</span>
-    <span class="name">${pokemon.name}</span>
-    <div class="detail">
-            <img src=${pokemon.photo} alt=${pokemon.name}>
-            
-        <ol class="types"> 
-        ${pokemon.types.map((typeName) => `<li class="type">${typeName}</li>`).join('')}
-        </ol>
-    </div>
+loadMorePokemons()
 
-    </li>`
-}
-
-
-pokeApi.getPokemons().then((pokemons) => {
-    const newList = pokemons.map((convertPokemonToHtml)).join('')
-    pokemonList.innerHTML = newList
-})
+load.addEventListener('click', () => 
+{ 
+    offset += limit
+    const nextPage = offset + limit
+    if(nextPage >= maxFatch) 
+    {
+        const newLimit = maxFatch - offset
+        loadMorePokemons(offset,newLimit)
+        load.parentElement.removeChild(load) 
+    }
+    else  loadMorePokemons(offset,limit)})
 
